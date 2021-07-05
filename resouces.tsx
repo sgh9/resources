6b469aea193b09203e87bfb3bfdbcef7925fe5cc
sgh9 <58831342+sgh9@users.noreply.github.com>
2021-07-05 18:59:35 +0530

    Update resouces.tsx

--- resouces.tsx
+++ resouces.tsx
@@ -1,2 +1,5 @@
 How do you put an image file in a json object?-
 https://stackoverflow.com/questions/34485420/how-do-you-put-an-image-file-in-a-json-object
+
+Build an E-Commerce website with MERN Stack - Part 1 (Setting Up the Project
+https://dev.to/shubham1710/build-an-e-commerce-website-with-mern-stack-part-1-setting-up-the-project-1l2d
