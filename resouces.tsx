9636ccfb529feef5808d3f3f10e6de2bb6029c22
sgh9 <58831342+sgh9@users.noreply.github.com>
2021-08-18 12:11:31 +0530

    added file upload handling link

--- resouces.tsx
+++ resouces.tsx
@@ -3,3 +3,5 @@ https://stackoverflow.com/questions/34485420/how-do-you-put-an-image-file-in-a-j
 
 Build an E-Commerce website with MERN Stack - Part 1 (Setting Up the Project
 https://dev.to/shubham1710/build-an-e-commerce-website-with-mern-stack-part-1-setting-up-the-project-1l2d
+
+https://stackoverflow.com/questions/39695275/react-js-handling-file-upload
